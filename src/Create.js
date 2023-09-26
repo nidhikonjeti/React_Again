8f9d1fde8e191f621a5f427e173e7cedf9bff69b
Sonuuuuuuu <sonynidhikonjeti5@gmail.com>
2023-09-26 22:19:14 +0530

    added

--- src/Create.js
+++ src/Create.js
@@ -1,55 +1,55 @@
 import { useState } from "react";
-import {useHistory} from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-    const [title,setTitle]=useState('')
-    const [body,setBody]=useState('')
-    const [author,setAuthor]=useState('nidhi')
-    const histroy=useHistory();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const history = useHistory();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
 
-    const handleSubmit=(e)=>
-    {
-        e.preventDefault();
-        const blog={title,body,author};
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      history.push('/');
+    })
+  }
 
-        fetch('http://localhost:8000/blogs',
-        {
-        
-            method:'POST',
-            headers:{"content-type":"application/json"},
-            body:JSON.stringify(blog)
-
-        }).then(()=>
-        {
-            console.log('new blog added')
-            
-            //histroy.go(-1)
-            histroy.push('/')
-        })
-    }
-    return ( 
-        <div className="create">
-         <h2>add a new blog</h2>
-         <form onSubmit={handleSubmit}>
-            <label >BLog Title:</label>
-            <input type="text"
-            required
-            value={title}
-            onChange={(e)=>setTitle(e.target.value)}/>
-            <label>Blog body:</label>
-            <textarea value={body} onChange={(e)=>setBody(e.target.value)}required></textarea>
-            <label>Blog Author:</label>
-            <select
-            value={author}
-            onChange={(e)=>setAuthor(e.target.value)}>
-                <option value="nidhi">Mario</option>
-                <option value="lode">lode</option>
-            </select>
-            <button>Add Blog</button>
-         </form>
-        </div>
-     );
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 }
  
 export default Create;
--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,16 @@
-
-import BlogList from './BlogList';
-import useFetch from './useFetch';
-
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-     
-    const {data:blogs,ispending,error}=useFetch('http://localhost:8000/blogs')
-    
-    
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    
-
-    
-    return ( 
-        <div className='home'>
-            {error && <div>{error}</div>}
-            {ispending && <div>loading..</div>}
-            {blogs && <BlogList blogs={blogs} title="all blogs" />}
-            
-
-
-        
-        </div>
-     );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
-export default Home;
-
-
-
-     //used to re-render the data.sometimes used to fetch data and for authentication
-     //we use if because it takes time for react to load and initially the blog is set to null.so until the condition becomes true the output wont be displayed
-     // when a state is changed the useeffect rerenders the data 
-     //only runs once for empty dependency array
-     //renders whenever there is change in the state of name
  
+export default Home;
